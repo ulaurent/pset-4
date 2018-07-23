f5159b1a7677b0f63d4b3eb63fdeda0962a6dedb
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-23 01:40:51 +0000

    initial commit with command line acceptance

---
+++ whodunit/whodunit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (int argc, char *argv[]){
+
+    if (argc != 3){
+
+        fprintf(stderr, "Usage: Error with command line arg. amounts");
+
+        return 1;
+    }
+
+    // Name of input file to open for reading
+    // & Nam eof output file to open for writing
+
+}
