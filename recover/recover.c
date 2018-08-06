4587f8f8a124925a2585fa406b09a34598b424e2
Urbenson Laurent <ulaurent12@gmail.com>
2018-08-06 00:07:33 +0000

    recovering deleted jpeg files

--- recover/recover.c
+++ recover/recover.c
@@ -1,63 +1,79 @@
-/* Program used to recover deleted jpeg files */
+/* Recovering a deleted JPEG File */
 
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
 
-
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-
+    // if command line arguments does not equal 2 return an error
     if (argc != 2)
     {
-        printf("Usage: ./recover filename\ns");
-    }
-
-    //Get file from command line args
-    char *raw_file = argv[1];
-
-    // open input file
-    FILE *file = fopen(raw_file, "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Could not open %s.\n", file1);
+        fprintf(stderr, "Usage: ./recover argument\n");
         return 1;
     }
 
-    //BYTE type is used to create a BYTE array
+    // set command line argument into variable
+    char *raw_file = argv[1];
+
+    // Open the file
+    FILE *file = fopen (raw_file, "r");
+
+    // Check if file open is not equal to NULL
+    if (file == NULL)
+    {
+        fprintf(stderr, "File Not opened");
+        return 2;
+    }
+
     typedef uint8_t BYTE;
     BYTE buffer[512];
-    FILE *img = NULL;
-    int img_counter = 0;
-
-    // Remember that filename is a string with 7 characters
+    FILE *image = NULL;
+    // Used to store new filename names created using 'sprintf'
     char filename[8];
+    // Image count increases to change jpeg filename each time
+    int img_count = 0;
 
-    // If fread returns a value that is not 512, it signifies EOF
-    while (fread(&buffer, 1, 512, file) == 512)
+    // Now we want to read into file while it hasnt reached EOF (end of file)
+    while (fread(buffer, 1, 512, file) == 512)
     {
-        // Check for JPEG header
+        // If this condition passes, we know what we are reading into is a header of a Jpeg
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // If header exists, check if a photo has been found yet
-            if (img != NULL)
+            // When we hit a JPEGS header, we must start a new file,
+            // To check if we are in an image file already created and open, we have to close it
+            // before creating a new image file
+
+            if (image != NULL) // We know if its not equal to null it is an open file with values in it so we must close the file
             {
-                // If a photo was already found, close it before writing a new jpg file
-                fclose(img);
+                fclose(image);
             }
 
-            sprintf(filename, "%03i.jpg", img_counter);
-            img_counter++;
-            img = fopen(filename, "w");
-            fwrite(buffer, 1, 512, img);
+            // Now if it is equal to NULL we know the image hasnt been created yet
+            // Now we want to write the block to an out put file called image
+            // creating a new output file each go around
+
+            // string print file name to store in a variable filename // img count changes file name each time
+            sprintf(filename, "%03i.jpg", img_count);
+            img_count ++;
+
+            // Open up new image file created
+            image = fopen(filename, "w");
+
+            // Then write block to image file created
+            fwrite(buffer, 1, 512, image);
         }
-        // If no header is read in this block, then write it to the current jpg
+
+        // Else if its not a header of a JPEG file, we know we can just write it to the current image file open
         else
         {
-            //  Make sure we have opened a file already
-            if (img != NULL)
+            // If image is not equal to null we know it is an open file, so we can just write to it
+            if (image != NULL)
             {
-                fwrite(buffer, 1, 512, img);
+                fwrite(buffer, 1, 512, image);
             }
         }
+
     }
+
+
 }
